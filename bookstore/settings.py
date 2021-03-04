f5fdf17d8a94b643d0fff48315b415ce6733f651
RaymondWhitfield <77745220+RaymondWhitfield@users.noreply.github.com>
2021-03-04 11:54:09 -0600

    Update settings.py

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'whh*4$vg79e-lg&35#!q+27)jtt0*@@$g36)ntgj1=4f-9s_te'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
